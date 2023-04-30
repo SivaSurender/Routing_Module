515caa68b767d4b451e580a8fd409c85eccdcd38
SivaSurender <e.surender14@gmail.com>
2023-04-30 14:37:28 +0530

    added code

--- src/UI/Dropdown.jsx
+++ src/UI/Dropdown.jsx
@@ -1,19 +1,44 @@
 import React, { useState } from "react";
 
 function Dropdown({ options }) {
-  const [dropDownData, setDropDownData] = useState({ isDropOpen: false });
+  const [dropDownData, setDropDownData] = useState({
+    isDropOpen: false,
+    selectedValue: "",
+  });
 
   const dropHandler = function () {
     setDropDownData((prev) => {
       return { ...prev, isDropOpen: !prev.isDropOpen };
     });
   };
+
+  const selectHandler = function (option) {
+    setDropDownData((prev) => {
+      return { ...prev, isDropOpen: false };
+    });
+
+    setDropDownData((prev) => {
+      return { ...prev, selectedValue: [option.label] };
+    });
+  };
   return (
     <div>
-      <div onClick={() => dropHandler()}>Select an option</div>
+      <div style={{ cursor: "pointer" }} onClick={() => dropHandler()}>
+        {dropDownData.selectedValue.length <= 0
+          ? "Select an option"
+          : dropDownData.selectedValue}
+      </div>
       {dropDownData.isDropOpen &&
         options.map((each, index) => {
-          return <div key={each.value}>{each.label}</div>;
+          return (
+            <div
+              style={{ cursor: "pointer" }}
+              onClick={() => selectHandler(each)}
+              key={each.value}
+            >
+              {each.label}
+            </div>
+          );
         })}
     </div>
   );
