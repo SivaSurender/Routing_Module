37c68cd1faea571e9378ec5eb8366d4c5ed13dd1
SivaSurender <e.surender14@gmail.com>
2023-04-30 12:12:22 +0530

    added code

--- src/UI/Accordion.jsx
+++ src/UI/Accordion.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from "react";
+import { GoChevronLeft, GoChevronDown } from "react-icons/go";
 
 function Accordion({ item }) {
   const [expandedIndex, setExpandedIndex] = useState(-1);
 
+  const handeAccordion = function (incomingIndex) {
+    setExpandedIndex((prev) => {
+      return incomingIndex !== prev ? incomingIndex : -1;
+    });
+  };
+
   return (
-    <div>
+    <div className="border-x border-t rounded">
       {item.map((eachItem, index) => {
         const isExpanded = index === expandedIndex;
         return (
           <div key={eachItem.id}>
-            <div onClick={() => setExpandedIndex(isExpanded ? -1 : index)}>
-              {eachItem.label}
+            <div
+              className="flex p-3 bg-gray-50 border-b items-center cursor-pointer justify-between"
+              onClick={() => handeAccordion(index)}
+            >
+              <span>{eachItem.label}</span>
+              <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
             </div>
-            {isExpanded && <div>{eachItem.content}</div>}
+            {isExpanded && (
+              <div className="border-b p-5">{eachItem.content}</div>
+            )}
           </div>
         );
       })}
