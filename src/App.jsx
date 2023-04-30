e6107ec834f10f4b8a5507f606c0c554ed8f93b5
SivaSurender <e.surender14@gmail.com>
2023-04-30 11:22:33 +0530

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,21 @@ import Accordion from "./UI/Accordion";
 const item = [
   {
     id: "sdfs43",
-    label: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+    label: "Content1",
     content:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur ullam distinctio atque vel. Dicta delectus molestias deserunt corporis. Debitis deserunt officiis, cum beatae voluptate incidunt tenetur veritatis culpa labore voluptas",
   },
 
   {
     id: "343dsdf",
-    label: "content2",
+    label: "Content2",
     content:
       " Debitis, minus voluptate ex, animi blanditiis nobis alias tenetur error laboriosam illum maiores. Cupiditate debitis consequatur et adipisci quas magnam consectetur eni",
   },
 
   {
     id: "sad687",
-    label: "content3",
+    label: "Content3",
     content:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium id perspiciatis cum nam iure nulla ab omnis. Aliquam enim cum laborum. Cupiditate nisi aut numquam, nam molestiae aperiam rerum quam!",
   },
--- src/UI/Accordion.jsx
+++ src/UI/Accordion.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Accordion({ item }) {
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+
   return (
     <div>
-      {item.map((eachItem) => (
-        <div key={eachItem.id}>
-          <div>{eachItem.label}</div>
-          <div>{eachItem.content}</div>
-        </div>
-      ))}
+      {item.map((eachItem, index) => {
+        const isExpanded = index === expandedIndex;
+        return (
+          <div key={eachItem.id}>
+            <div onClick={() => setExpandedIndex(isExpanded ? -1 : index)}>
+              {eachItem.label}
+            </div>
+            {isExpanded && <div>{eachItem.content}</div>}
+          </div>
+        );
+      })}
     </div>
   );
 }
