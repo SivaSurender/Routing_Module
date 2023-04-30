1a03dbd0a53860bbce4840c24711efb2022d15a7
SivaSurender <e.surender14@gmail.com>
2023-04-30 15:37:14 +0530

    added code

--- src/UI/Dropdown.jsx
+++ src/UI/Dropdown.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
 
 function Dropdown({ options }) {
   const [dropDownData, setDropDownData] = useState({
@@ -18,28 +19,36 @@ function Dropdown({ options }) {
     });
 
     setDropDownData((prev) => {
-      return { ...prev, selectedValue: [option.label] };
+      return { ...prev, selectedValue: option.label };
     });
   };
   return (
-    <div>
-      <div style={{ cursor: "pointer" }} onClick={() => dropHandler()}>
-        {dropDownData.selectedValue.length <= 0
-          ? "Select an option"
-          : dropDownData.selectedValue}
+    <div className="w-48 relative">
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+        onClick={() => dropHandler()}
+      >
+        {dropDownData.selectedValue
+          ? dropDownData.selectedValue
+          : "Select an option"}
+
+        <GoChevronDown className="text-lg" />
       </div>
-      {dropDownData.isDropOpen &&
-        options.map((each, index) => {
-          return (
-            <div
-              style={{ cursor: "pointer" }}
-              onClick={() => selectHandler(each)}
-              key={each.value}
-            >
-              {each.label}
-            </div>
-          );
-        })}
+      {dropDownData.isDropOpen && (
+        <div className="absolute top-full border rounded p-3 shadow  bg-white w-full">
+          {options.map((each, index) => {
+            return (
+              <div
+                className="hover:bg-sky-200 rounded cursor-pointer p-1"
+                onClick={() => selectHandler(each)}
+                key={each.value}
+              >
+                {each.label}
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
