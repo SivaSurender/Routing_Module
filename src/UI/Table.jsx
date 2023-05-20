d905ae4d9e3cdaeb9fae6dfa7c5fda4bfef6c513
SivaSurender <e.surender14@gmail.com>
2023-05-20 18:03:35 +0530

    added code

--- src/UI/Table.jsx
+++ src/UI/Table.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 
 function Table({ data }) {
-  console.log(data);
-  return <div>Table</div>;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Fruit</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((each, index) => {
+          return (
+            <tr key={index}>
+              <td>{each.name}</td>
+              <td>{each.color}</td>
+              <td>{each.score}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
 
 export default Table;
